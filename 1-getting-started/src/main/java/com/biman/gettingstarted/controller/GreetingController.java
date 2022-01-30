491c451f196b59219ce9884018c77bc3883ca00c
bimanmandal <biman29@gmail.com>
2022-01-30 18:54:38 +0530

    added few more endpoints in getting started

--- 1-getting-started/src/main/java/com/biman/gettingstarted/controller/GreetingController.java
+++ 1-getting-started/src/main/java/com/biman/gettingstarted/controller/GreetingController.java
@@ -7,11 +7,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class GreetingController {
     @CrossOrigin(origins = "*")
     @GetMapping("/hello")
     public String greeting(@RequestParam String name){
         return "Hello "+ name;
+    }
+
+    @GetMapping("/college")
+    public List<String> getStudents() {
+        List<String> students =  new ArrayList<>();
+        students.add("Sourav");
+        students.add("Ishan");
+        students.add("Debojit");
+        return students;
+    }
+
+    @GetMapping("/sum")
+    public int sum(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+
+    @GetMapping("/uppercase")
+    public String upper(@RequestParam String str) {
+        return str.toUpperCase();
     }
 }
